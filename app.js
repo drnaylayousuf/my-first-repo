31fc3a8f000554c3b0968635562ae5ad97ccefac
drnaylayousuf <naylayousuf.dr1310@gmail.com>
2023-09-08 00:13:03 +0500

    first assignment

---
+++ app.js
@@ -0,0 +1,162 @@
+//  chapter 1
+// Q1 
+// I.
+// var firstName = "Nayla";
+// alert(firstName);
+// // II.
+// var lastName = "Yousuf";
+// alert(lastName);
+// // III.
+// var email = "naylayousuf@gmail.com";
+// alert("Email: " + email);
+// // IV.
+// var phoneNumber = "922222222222";
+// alert("phoneNumber: " + phoneNumber);
+// // V.
+// var password = "abcd123";
+// alert("password: " + password);
+
+// Q2 statement
+// alert("You re' learning javascript!");
+
+// Q3
+// alert("Hello! World");
+
+
+// chapter 2
+// Q1
+
+// var userName= "Nayla Yousuf";
+
+// Q2
+
+// var nickName;
+// nickName = "neon";
+
+
+// Q3
+// var teamName;
+// teamName= "Rock-band";
+// alert(teamName);
+
+
+// Q4
+
+// var bestMan="Charlie";
+
+// bestMan = "Lewis";
+
+
+// CHAPTER 3
+
+// Q1
+
+// var caseQty;
+
+// Q2
+// caseQty = 144;
+
+// Q3
+
+// var num = 9;
+
+
+// Q4
+
+// var number;
+
+// number = 2 + 7;
+
+
+// Q5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+
+// Q6
+
+// var students = 20;
+// var calculate = students + 20;
+// console.log(calculate);
+
+//  another one
+// var students = 20;
+// students = students + 20;
+
+
+
+// CHAPTER 4
+
+// Q1
+
+
+// var productcost = 3.45;
+
+// Q2
+
+// var nameOfBand;
+
+// Q3
+
+// var num_1 = 5;
+
+// Q4
+
+// var naylaYousuf;
+
+// Q5
+
+// LEGAL VARIBLES
+
+// var naylaYousuf; //   camel case
+// var nayla_Yousuf;
+// var naylayousuf;
+// var naylayousuf123;
+
+
+// ILLEGAL VARIABLES
+
+// var 123naylaYousuf;
+// var nayla-Yousuf;
+// var nayla yousuf;
+// var nayla&yousuf123;
+// var 123;
+
+
+
+// CHAPTER 5
+
+// Q1
+
+//   ( %  modulus  )
+
+// Q2
+
+// var num = 20 % 6;
+// console.log(num)
+//  num = 2             (answer)
+
+// Q3
+
+// var largeNum = 1000 * 2000;
+// console.log(largeNum);
+// largeNum = 2000000             (answer)
+
+
+// Q4
+
+// totalProfit = itemcost - itemprice;
+
+
+// Q5
+
+// var leftOver = 5 % 6;
+
+// Q6
+
+// var cost1 = 7;
+// var cost2 = 9;
+// alert(cost1 * cost2);
